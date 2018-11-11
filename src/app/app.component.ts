21ea3c7c0ce6fc7d84dceebbc88cd3fa646aafb9
Eric Fiorim <ericfiorim@Erics-MacBook-Pro.local>
2018-11-11 13:24:32 +0000

    base style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { StarshipService } from 'src/app/starship.service';
 import { Starship } from './interfaces/starship.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
+
 })
 export class AppComponent {
   title = 'Starships';
