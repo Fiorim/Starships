81c04b26b482174a33244790fb8464c7c1f8089a
Eric Fiorim <ericfiorim@Erics-MacBook-Pro.local>
2018-11-11 02:33:42 +0000

    starship service

---
+++ src/app/starship.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { SWAPIStarship } from 'src/app/interfaces/swapi-starship.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StarshipService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getStarshipList(): Observable<SWAPIStarship> {
+
+    return this.httpClient.get('https://swapi.co/api/starships/') as Observable<SWAPIStarship>
+
+  }
+}
+
+
